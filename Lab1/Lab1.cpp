d8b6b2436e84d4e777535e11d20ca3f1f35be513
unki113d <unkilled135@gmail.com>
2024-02-23 13:26:07 +0200

    Added common logic for horizontal line drawer. Also added all functions prototypes

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -6,20 +6,63 @@
 
 using  namespace std;
 
+void DrawHorizontalLine(int, char);
+void DrawVerticalLine(int height, char ch);
+void DrawSquare(int size, char ch);
+void DrawRectangle(int height, int length, char ch);
+
 int main()
 {
 	int choice;
+	int _length;
+	char _ch;
 	do
 	{
 
-	cout << "\n1) Draw a horizontal line";
-	cout << "\n2) Draw a vertical line";
-	cout << "\n3) Draw a square";
-	cout << "\n4) Draw a rectangle";
-	cout << "\n5) Quit";
-	cout << "\nEnter choice: ";
-	cin >> choice;
+		cout << "\n1) Draw a horizontal line";
+		cout << "\n2) Draw a vertical line";
+		cout << "\n3) Draw a square";
+		cout << "\n4) Draw a rectangle";
+		cout << "\n5) Quit";
+		cout << "\nEnter choice: ";
+		cin >> choice;
+
+
+		switch (choice)
+		{
+		case 1:
+			cout << "\n\nEnter length of the line : ";
+			cin >> _length;
+
+			cout << "\nEnter a character of the line: ";
+			cin >> _ch;
+			DrawHorizontalLine(_length, _ch);
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		default:
+			break;
+		}
+
 	} while (choice != 5);
 
 	
 }
+
+//Draws a horizontal line
+void DrawHorizontalLine(int length, char ch)
+{
+	for (int i = 0; i < length; ++i)
+	{
+		cout << ch;
+	}
+	cout << endl;
+}
+
